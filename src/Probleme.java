49afc3bb27055e8289d3b2c9f4fc5d74a5d0e655
derivas <derivas@etud.insa-toulouse.fr>
2018-04-04 09:11:27 +0200

    ajout classes

---
+++ src/Probleme.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class Probleme {
+
+    public Node nodestart ;
+    public Node nodeend ;
+    public List<Machine> lesMachines ;
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
